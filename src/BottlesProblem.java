7fe564d75832bf0bf6f2633c85f77ed17e43d41d
elad11310 <elad11310@gmail.com>
2020-03-31 23:05:40 +0300

    lesson 2 - added some functions can be found in description

--- src/BottlesProblem.java
+++ src/BottlesProblem.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
 
-import static sun.nio.cs.Surrogate.MIN;
 
 /**
  * this class represents bottles problem which converted to a graph
@@ -19,43 +17,168 @@ import static sun.nio.cs.Surrogate.MIN;
  * 1) open a matrix in size of [(m+1)(n+1)] [(m+1)(n+1)]
  * 2) iterating through the mat send each cell(mat[i][j]) to a function that will convert i and j to ordered pair.
  * 3) the function will check according to m,n the 6 options depicted above. if one of them exists the function will return true.
+ * <p>
+ * adding some functions:
+ * 1)findWaysFW - an algorithm which gets a neighbour matrix and returns a matrix that will determine about each vertex if it can be reached from another vertex
+ * summary of this algorithm : basically we run through all the vertexes and checks if there a way between two of those, if now maybe through another
+ * vertex - mat[i,j] = mat[i,k] && mat[k,j] || mat[i,j],on the same hand we will make another matrix with all the vertexes courses.
+ * isConnected - checks if the graph is connected - means we can reach any vertex in the graph.
+ * numOfConnectingComponents - checks how many connecting components in the graph.
+ * checkWay - checks if there is a way between two vertexes and returns the way (enter number not by modeling to bottles problem,by normal numbers 0,1,2,3... - instead of (0,0) -> 0)
  */
 
 
 public class BottlesProblem {
-    static int n = 3, m = 2;
+    static int n = 5, m = 3;
     static int size = (m + 1) * (n + 1);
+    static String ways[][] = new String[size][size];
+
 
     public static void main(String[] args) {
 
-        boolean mat [][] = bottlesProblem(m,n);
-        // System.out.println(Arrays.deepToString(bottlesProblem(m, n)));
-        for (int i = 0; i <size;i++){
-            for (int j=0;j<size;j++){
-                System.out.print(mat[i][j]+" , ");
-            }
-            System.out.println();
-        }
+//        Boolean check [][] = {{true,false,false,true,false,true,false},
+//                {false,true,true,false,true,false,true},
+//                {false,true,true,false,true,false,true},
+//                {true,false,false,true,false,true,false},
+//                {false,true,true,false,true,false,true},
+//                {true,false,false,true,false,true,false},
+//                {false,true,true,false,true,false,true}
+//        };
+
+        Boolean neighbourMat[][] = bottlesProblem();
+        System.out.println("Neighbour matrix");
+        printAll(neighbourMat);
+        Boolean checkConnections[][] = bottlesProblem();
+        checkConnections = findWaysFW(checkConnections, neighbourMat);
+        System.out.println("Courses matrix");
+        printAll(checkConnections);
+        System.out.println();
+        printAll(ways);
+        System.out.println();
+        System.out.println("Graph connected : " + isConnected(checkConnections));
+        System.out.println();
+         System.out.println(checkWay(checkConnections, 6, 4));
+        System.out.println("Num of connecting component : " + numOfConnectingComponents(checkConnections));
+
     }
 
-    private static boolean[][] bottlesProblem(int m, int n) {
+    private static int numOfConnectingComponents(Boolean[][] mat) {
+        int i, j;
+        boolean anotherConnection=false; // to know if there is a new connection component
+        int count = 0; // to count how many components
+        int notEmpty = 0; // to know if its not the connection we already defined before.
 
-        boolean[][] mat = new boolean[size][size];
+        int arr[] = new int[size]; // putting all values to -1 because we need to mark 0 as well too.
+        for (i = 0; i < size; i++) {
+            arr[i] = -1;
+        }
+
+
+        for (i = 0; i < size; i++) {
+            for (j = 0; j < size; j++) {
+                if (mat[i][j]) {
+                    if (arr[j] == -1) {
+                        notEmpty++;
+                        anotherConnection=true;
+                    }
+                    arr[j] = i;
+                }
+            }
+            if (anotherConnection) {
+                count++;
+                anotherConnection=false;
+            }
+            if (notEmpty == size) {
+                return count;
+            }
+
+        }
+
+        return 0;
+    }
+
+    private static String checkWay(Boolean[][] mat, int i, int j) {
+        String ans = "";
+        if (mat[i][j]) { // if there is a way
+            String vertex[] = ways[i][j].split(" -> ");
+            for (int k = 0; k < vertex.length; k++) {
+                // converting the ordered pair to k
+                int p = Integer.parseInt(vertex[k].charAt(1) + "");
+                int q = Integer.parseInt(vertex[k].charAt(3) + "");
+                int num = (n + 1) * p + q;
+                ans += num + " -> ";
+
+            }
+        } else return "There is no course between these vertexes";
+        ans = ans.substring(0, ans.length() - 4); // cut last ->
+        return ans;
+    }
+
+    private static boolean isConnected(Boolean[][] mat) {
+        for (int i = 0; i < size; i++) {
+            if (!mat[0][i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static Boolean[][] findWaysFW(Boolean[][] checkConnections, Boolean[][] neighbourMat) {
+        int p, l, g, q;
+
+
+        // O(n^3)
+        for (int k = 0; k < size; k++) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    // p,i ordered pair of i
+                    p = i / (n + 1);
+                    l = i % (n + 1);
+                    // g,q ordered pair of j
+                    g = j / (n + 1);
+                    q = j % (n + 1);
+
+                    // checks if there is any vertex k that connects between i and j or they are already connected by one edge
+                    checkConnections[i][j] = (checkConnections[i][k] && checkConnections[k][j]) || checkConnections[i][j];
+
+                    if (neighbourMat[i][j] && ways[i][j] == null) { // means we already have a course by 1 edge
+                        ways[i][j] = "(" + p + "," + l + ") -> (" + g + "," + q + ")";
+                    } else if (checkConnections[i][k] && checkConnections[k][j] && !neighbourMat[i][j]) { // if there is  more then one  edge course
+                        neighbourMat[i][j] = true;
+                        if (ways[i][j] == null) {
+                            ways[i][j] = ways[i][k] + " -> " + ways[k][j].substring(9);
+                        } else {
+                            ways[i][j] += ways[i][k] + " -> " + ways[k][j];
+                        }
+                    }
+
+
+                }
+            }
+
+
+        }
+        return checkConnections;
+    }
+
+    private static Boolean[][] bottlesProblem() {
+
+        Boolean[][] mat = new Boolean[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                mat[i][j] = checkWay(i, j);
+                mat[i][j] = checkNeighbours(i, j);
             }
         }
 
         return mat;
     }
 
-    private static boolean checkWay(int i, int j) {
+    private static boolean checkNeighbours(int i, int j) {
 
         // k ,p are ordered pair of i
         int k = i / (n + 1);
         int p = i % (n + 1);
-        // k ,p are ordered pair of j
+        // d ,j are ordered pair of j
         int d = j / (n + 1);
         int f = j % (n + 1);
 
@@ -83,5 +206,14 @@ public class BottlesProblem {
 
     private static int MIN(int i, int n) { // min method.
         return i < n ? i : n;
+    }
+
+    private static <T> void printAll(T[][] t) {
+        for (int i = 0; i < t.length; i++) {
+            for (int j = 0; j < t.length; j++) {
+                System.out.print(t[i][j] + ",");
+            }
+            System.out.println();
+        }
     }
 }
